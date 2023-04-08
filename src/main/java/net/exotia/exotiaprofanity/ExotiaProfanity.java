9de6efe145d606011900074927de02fc88ffb616
Wiktor Wilk <mcserw4@wp.pl>
2023-04-08 21:46:50 +0200

    initial commit

---
+++ src/main/java/net/exotia/exotiaprofanity/ExotiaProfanity.java
@@ -0,0 +1,17 @@
+package net.exotia.exotiaprofanity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExotiaProfanity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
